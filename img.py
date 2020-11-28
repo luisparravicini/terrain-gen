ef2b08f82f7250e404cc16385465f8db93781981
Luis Parravicini <lparravi@gmail.com>
2020-11-28 20:25:03 -0300

    using pillow to create the images

--- img.py
+++ img.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from perlin_noise import PerlinNoise
+from PIL import Image
 
 # noise = PerlinNoise(octaves=10, seed=1)
 # xpix, ypix = 100, 100
@@ -14,17 +15,32 @@ noise3 = PerlinNoise(octaves=12)
 noise4 = PerlinNoise(octaves=24)
 
 xpix, ypix = 100, 100
-pic = []
-for i in range(xpix):
-    row = []
-    for j in range(ypix):
-        noise_val =         noise1([i/xpix, j/ypix])
-        noise_val += 0.5  * noise2([i/xpix, j/ypix])
-        noise_val += 0.25 * noise3([i/xpix, j/ypix])
-        noise_val += 0.125* noise4([i/xpix, j/ypix])
+img = Image.new('L', size=(xpix, ypix), color='black')
+for i in range(img.size[0]):
+    for j in range(img.size[1]):
+        noise_val = noise1([i/xpix, j/ypix])
+        # noise_val += 0.5 * noise2([i/xpix, j/ypix])
+        # noise_val += 0.25 * noise3([i/xpix, j/ypix])
+        # noise_val += 0.125 * noise4([i/xpix, j/ypix])
 
-        row.append(noise_val)
-    pic.append(row)
+        color = (int)(noise_val * 255)
 
-plt.imshow(pic, cmap='gray')
-plt.show()
+        img.putpixel((i, j), color)
+
+# pic = []
+# for i in range(xpix):
+#     row = []
+#     for j in range(ypix):
+#         noise_val =         noise1([i/xpix, j/ypix])
+#         noise_val += 0.5  * noise2([i/xpix, j/ypix])
+#         noise_val += 0.25 * noise3([i/xpix, j/ypix])
+#         noise_val += 0.125* noise4([i/xpix, j/ypix])
+
+#         row.append(noise_val)
+#     pic.append(row)
+
+# plt.imshow(pic, cmap='gray')
+# plt.show()
+
+
+img.save('VoxelSpace/maps/terrain-1.png')
