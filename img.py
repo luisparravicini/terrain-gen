d72d20b333d7e164a72d718f112656456b9aaf5a
Luis Parravicini <lparravi@gmail.com>
2020-11-29 06:52:39 -0300

    setting all the image data at once (faster)

--- img.py
+++ img.py
@@ -8,7 +8,9 @@ noise4 = PerlinNoise(octaves=24)
 
 xpix, ypix = 100, 100
 img = Image.new('L', size=(xpix, ypix), color='black')
+data = []
 for i in range(img.size[0]):
+    row = []
     for j in range(img.size[1]):
         noise_val = noise1([i/xpix, j/ypix])
         # noise_val += 0.5 * noise2([i/xpix, j/ypix])
@@ -17,6 +19,9 @@ for i in range(img.size[0]):
 
         color = (int)(noise_val * 255)
 
-        img.putpixel((i, j), color)
+        row.append(color)
+    data.append(row)
+
+img.putdata(data)
 
 img.save('maps/terrain-1.png')
