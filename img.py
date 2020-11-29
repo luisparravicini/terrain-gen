60be21c10f4b725f70f28dacf997bca2fc10cdb0
Luis Parravicini <lparravi@gmail.com>
2020-11-28 21:08:05 -0300

    added MIT license

--- img.py
+++ img.py
@@ -1,13 +1,5 @@
-# import matplotlib.pyplot as plt
 from perlin_noise import PerlinNoise
 from PIL import Image
-
-# noise = PerlinNoise(octaves=10, seed=1)
-# xpix, ypix = 100, 100
-# pic = [[noise([i/xpix, j/ypix]) for j in range(xpix)] for i in range(ypix)]
-
-# plt.imshow(pic, cmap='gray')
-# plt.show()
 
 noise1 = PerlinNoise(octaves=3)
 noise2 = PerlinNoise(octaves=6)
@@ -26,21 +18,5 @@ for i in range(img.size[0]):
         color = (int)(noise_val * 255)
 
         img.putpixel((i, j), color)
-
-# pic = []
-# for i in range(xpix):
-#     row = []
-#     for j in range(ypix):
-#         noise_val =         noise1([i/xpix, j/ypix])
-#         noise_val += 0.5  * noise2([i/xpix, j/ypix])
-#         noise_val += 0.25 * noise3([i/xpix, j/ypix])
-#         noise_val += 0.125* noise4([i/xpix, j/ypix])
-
-#         row.append(noise_val)
-#     pic.append(row)
-
-# plt.imshow(pic, cmap='gray')
-# plt.show()
-
 
 img.save('VoxelSpace/maps/terrain-1.png')
